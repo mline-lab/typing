8be30db46eada4e3fdfe2ebabb24f9d82bf9a82e
愛希 <manaki079@gmail.com>
2017-01-19 13:49:33 +0900

    全キャラシステム追加

---
+++ char/js/yukino_bcv.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+  var result = null;
+
+  var cookieName = "yukino_cv" + '=';
+  var allcookies = document.cookie;
+
+  var position = allcookies.indexOf(cookieName);
+  if (position != -1) {
+      var startIndex = position + cookieName.length;
+
+      var endIndex = allcookies.indexOf(';', startIndex);
+      if (endIndex == -1) {
+          endIndex = allcookies.length;
+      }
+      result = decodeURIComponent(allcookies.substring(startIndex, endIndex));
+  }
+  document.getElementById("cv").value = result;
+}
+
+function bcv_set() {
+    cv = document.getElementById("cv").value;
+    var expire = new Date();
+    expire.setTime(expire.getTime() + 60 * 24 * 60 * 60 * 1000);
+
+    document.cookie = "yukino_cv=" + cv + "; expires=" + expire.toUTCString();
+}
