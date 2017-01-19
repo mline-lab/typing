ed5626223539d6a795c0581bdbff08e8cc914bee
愛希 <manaki079@gmail.com>
2017-01-19 13:43:15 +0900

    test

--- js/bcv.js
+++ js/bcv.js
@@ -1,3 +1,22 @@
+window.onload = function() {
+  var result = null;
+
+  var cookieName = "chiharu_cv" + '=';
+  var allcookies = document.cookie;
+
+  var position = allcookies.indexOf(cookieName);
+  if (position != -1) {
+      var startIndex = position + cookieName.length;
+
+      var endIndex = allcookies.indexOf(';', startIndex);
+      if (endIndex == -1) {
+          endIndex = allcookies.length;
+      }
+      result = decodeURIComponent(allcookies.substring(startIndex, endIndex));
+  }
+  document.getElementById("cv").value = result;
+}
+
 function bcv_set() {
     cv = document.getElementById("cv").value;
     var expire = new Date();
